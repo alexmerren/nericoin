d0564cd3dcfdf14e60a2ba6c3d0bf2ab344c3c5e
Jeremy Akhavi <jeremyakhavi@me.com>
2022-04-28 22:54:13 +0100

    Decoupled the database (#15)

--- internal/nerichain/iterator.go
+++ internal/nerichain/iterator.go
@@ -1,10 +1,7 @@
 package nerichain
 
 import (
-	"errors"
 	"nericoin/internal/neri"
-
-	"github.com/boltdb/bolt"
 )
 
 type NerichainIterator struct {
@@ -19,54 +16,23 @@ func NewNerichainIterator(n *Nerichain) *NerichainIterator {
 	}
 }
 
-// View the database and get the serialized neri with the currentHash in the
-// iterator.  Deserialize the neri and return that neri.
+// View the database and get the neri with the currentHash in the
+// iterator. Return that neri.
 func (i *NerichainIterator) GetNext() *neri.Neri {
 	if !i.HasNext() {
 		return nil
 	}
+	nextNeri, _ := i.nerichain.db.GetNeri(i.currentHash)
+	i.currentHash = nextNeri.PreviousHash
 
-	var nextNeri *neri.Neri
-	err := i.nerichain.database.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(blocksBucket))
-		serializedNeri := bucket.Get([]byte(i.currentHash))
-		neri := neri.Deserialize(serializedNeri)
-		if neri == nil {
-			return errors.New("could not deserialize current neri")
-		}
-		nextNeri = neri
-		i.currentHash = neri.PreviousHash
-		return nil
-	})
-	if err != nil {
-		return nil
-	}
 	return nextNeri
 }
 
-// View the database and find the serialized Neri that has the currentHash.
-// Then, Deserialize the Neri to get the previous hash, and the set the
-// iterators current hash as the next (previous) Neri in the chain.  If there
-// is no Neri attached to a hash, there is either an error or you've reached
-// the start.
+// Get neri with current hash from database and get the previous hash
+// and the set the iterators current hash as the next (previous) Neri
+// in the chain.  If there is no Neri attached to a hash, there is
+// either an error or you've reached the start.
 func (i *NerichainIterator) HasNext() bool {
-	err := i.nerichain.database.View(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte(blocksBucket))
-		serializedNeri := bucket.Get([]byte(i.currentHash))
-		// If the current neri does not exist in the DB
-		if serializedNeri == nil {
-			return errors.New("could not find the current hash")
-		}
-		// If the current neri cannot be deserialized
-		neri := neri.Deserialize(serializedNeri)
-		if neri == nil {
-			return errors.New("could not deserialize current neri")
-		}
-		// If the previous neri cannot be found in the DB
-		if len(neri.PreviousHash) == 0 {
-			return errors.New("cannot have length zero previous hash")
-		}
-		return nil
-	})
-	return err == nil
+	n, _ := i.nerichain.db.GetNeri(i.currentHash)
+	return n != nil
 }
